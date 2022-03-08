f9038249dfa860e6498946cf8015f736efac3b3a
cristinabarq <crisbarq@ucm.es>
2022-03-08 20:28:17 +0100

    Se crea el proyecto para la aplicación.

---
+++ recuerdame/resources/views/layout/header.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+  <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.css">
+  <script src="https://kit.fontawesome.com/d1ab37e54e.js" crossorigin="anonymous"></script>
+  <script src="bootstrap-5.1.3-dist/js/jquery.js"></script>
+  <script src="bootstrap-5.1.3-dist/js/bootstrap.js"></script>
+  <title>Recuerdame</title>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Recuerdame</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fa-solid fa-users"></i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fa-solid fa-envelope"></i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fa-solid fa-right-from-bracket"></i></a>
+          </li>
+        </ul>
+      </div>
+  </nav>
+</body>
+
+</html>
---
+++ recuerdame/resources/views/layout/nav.php
@@ -0,0 +1,45 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.css">
+    <script src="https://kit.fontawesome.com/d1ab37e54e.js" crossorigin="anonymous"></script>
+    <script src="bootstrap-5.1.3-dist/js/jquery.js"></script>
+    <script src="bootstrap-5.1.3-dist/js/bootstrap.js"></script>
+    <title>Recuerdame</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg justify-content-center">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Sesiones</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Evaluaciones</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Informes de seguimiento</a></li>
+                            <li><a class="dropdown-item" href="#">Evaluaciones</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Historias de Vida</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Generar Historia de Vida</a></li>
+                            <li><a class="dropdown-item" href="#">Recuerdos</a></li>
+                            <li><a class="dropdown-item" href="#">Personas relacionadas</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Calendario</a>
+                    </li>
+                </ul>
+            </div>
+    </nav>
+</body>
+
+</html>
