80f1d374dbe2205288a15bab45aa1424ea3e4813
PatriciaDG <patdiez@ucm.es>
2022-03-11 15:23:53 +0100

    Inicio de listaSesiones

--- recuerdame/index.php
+++ recuerdame/index.php
@@ -5,6 +5,7 @@
   <script src="https://kit.fontawesome.com/d1ab37e54e.js" crossorigin="anonymous"></script>
   <script src="resources/bootstrap-5.1.3-dist/js/bootstrap.js"></script>
   <title>Recuerdame</title>
+  <link rel="shortcut icon" type="image/x-icon" href="./resources/img/Logo_recuerdame_v2.ico" />
 </head>
 
 <body>
---
+++ recuerdame/listadoSesiones.php
@@ -0,0 +1,29 @@
+<html>
+    
+<head>
+  <link rel="stylesheet" href="resources/bootstrap-5.1.3-dist/css/bootstrap.css">
+  <script src="https://kit.fontawesome.com/d1ab37e54e.js" crossorigin="anonymous"></script>
+  <script src="resources/bootstrap-5.1.3-dist/js/bootstrap.js"></script>
+  <title>Recuerdame</title>
+  <link rel="shortcut icon" type="image/x-icon" href="./resources/img/Logo_recuerdame_v2.ico" />
+  <link rel="stylesheet" type="text/css" href="./resources/css/styles.css">
+</head>
+
+<body>
+	<?php include "./src/views/layout/header.php" ?>
+  <?php include "./src/views/layout/nav.php" ?>
+
+  <div class="cuerpo">
+			<div class = "busqueda">
+        <div class="buscar">
+          <div class="form-outline">
+            <input type="search" id="form1" class="form-control" placeholder="Buscar" aria-label="Search" />
+          </div>
+        </div>
+        <button type="button" class="btn btn-light">+</button>
+			</div>
+		</div>
+
+</body>
+
+</html>
