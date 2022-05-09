f59d49cc69aa33a5e5a005867ea0e3eb95c11ac0
cristinabarq <crisbarq@ucm.es>
2022-05-09 23:05:11 +0200

    Se añade la licencia.

--- recuerdame/layout/footer.php
+++ recuerdame/layout/footer.php
@@ -1,5 +1,6 @@
-<footer class="mt-auto text-center text-lg-start bg-light text-muted footer">
+<footer class="mt-auto text-lg-start bg-light text-muted footer">
     <div class="p-2">
-        © 2022 Copyright: Recuérdame
+        <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a>
+        <?php echo date('Y'); ?> Recuérdame
     </div>
 </footer>
