e420a876cfed60ce897c3d54d1d90dd5356c5228
cristinabarq <crisbarq@ucm.es>
2022-05-09 18:52:39 +0200

    Se modifican los estilos del registro del terapeuta.

--- recuerdame/registroTerapeuta.php
+++ recuerdame/registroTerapeuta.php
@@ -1,97 +1,60 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="public/bootstrap-5.1.3-dist/css/bootstrap.css">
-    <link rel="stylesheet" href="public/css/login.css">
-    <link rel="stylesheet" href="public/css/styles.css">
-    <link href="public/fontawesome6/css/all.css" rel="stylesheet">
-    <script src="public/bootstrap-5.1.3-dist/js/bootstrap.js"></script>
-    <script src="public/jquery/jquery-3.6.0.min.js"></script>
-    <script src="public/datatable/datatables.min.js"></script>
-    <link rel="stylesheet" href="public/datatable/datatables.min.css">
-    <script src="public/js/table.js"></script>
-    <link rel="shortcut icon" type="image/x-icon" href="public/img/Logo_recuerdame_v2.ico" />
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Recuérdame</title>
+  <link rel="stylesheet" href="public/bootstrap-5.1.3-dist/css/bootstrap.css">
+  <link rel="stylesheet" href="public/css/registro.css">
+  <link rel="stylesheet" href="public/css/styles.css">
+  <link href="public/fontawesome6/css/all.css" rel="stylesheet">
+  <script src="public/bootstrap-5.1.3-dist/js/bootstrap.js"></script>
+  <script src="public/jquery/jquery-3.6.0.min.js"></script>
+  <script src="public/datatable/datatables.min.js"></script>
+  <link rel="stylesheet" href="public/datatable/datatables.min.css">
+  <link rel="shortcut icon" type="image/x-icon" href="public/img/Logo_recuerdame_v2.ico" />
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Recuérdame</title>
 </head>
 
 <body class="d-flex flex-column min-vh-100">
-  
-<section class="vh-100" style="background-color: #eee;">
-  <div class="container h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-lg-12 col-xl-11">
-        <div class="card text-black" style="border-radius: 25px;">
-          <div class="card-body p-md-5">
-            <div class="row justify-content-center">
-              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+  <form action="gestorRegistroUsuario.php?rol=TERAPEUTA" method="POST" class="mx-1 mx-md-4">
+    <div class="card form-registro">
+      <div class="d-flex justify-content-center">
+        <img src="public/img/Marca_recuerdame.png" class="card-img-top logo">
+      </div>
+      <h5 class="text-center text-muted">Registro terapeuta</h5>
+      <?php if (isset($_GET['mensajeError'])) { ?>
+        <p style="color:red;" ;>El usuario o correo ya existe, pruebe otro</p>
+      <?php
+      }
+      ?>
+      <div class="d-flex flex-row align-items-center mb-4">
+        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+        <input type="text" id="apellidos" class="form-control form-control-sm" name="name" placeholder="Nombre" required>
+      </div>
 
-                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Registro terapeuta</p>
+      <div class="d-flex flex-row align-items-center mb-4">
+        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+        <input type="text" id="apellidos" class="form-control form-control-sm" name="apellidos" placeholder="Apellidos" required>
+      </div>
 
-                <form action="gestorRegistroUsuario.php?rol=TERAPEUTA" method= "POST" class="mx-1 mx-md-4">
-                      <?php if (isset($_GET['mensajeError'])){ ?>
-                        <p style="color:red;";>El usuario o correo ya existe, pruebe otro</p>
-                       <?php
-                        }
-                      ?>
-                    <div class="d-flex flex-row align-items-center mb-4">
-                      <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                        <input type="text" id="apellidos" class="form-control"  name="name"  required>
-                        <label class="form-label"for="form3Example1c">Nombre</label>
-                        </div>
-                    </div>
+      <div class="d-flex flex-row align-items-center mb-4">
+        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+        <input type="text" id="nick" class="form-control form-control-sm" name="nick" placeholder="Nombre de usuario" required>
+      </div>
 
-                    <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                        <input type="text" id="apellidos" class="form-control"  name="apellidos" required >
-                        <label class="form-label"for="form3Example1c">Apellidos</label>
-                        </div>
-                    </div>
+      <div class="d-flex flex-row align-items-center mb-4">
+        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+        <input type="email" id="email" class="form-control form-control-sm" name="mail" placeholder="Correo electrónico" required>
+      </div>
 
-                    <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                        <input type="text" id="nick" class="form-control" name="nick" required>
-                        <label class="form-label"  for="form3Example1c">Nombre de usuario</label>
-                        </div>
+      <div class="d-flex flex-row align-items-center mb-4">
+        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+        <input type="password" id="password" class="form-control form-control-sm" name="password" placeholder="Contraseña" required />
+      </div>
 
-                    </div>
-                    <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                        <input type="email" id="email" class="form-control" name= "mail" required>
-                        <label class="form-label" for="form3Example3c">Correo</label>
-                        </div>
-                    </div>
-
-                    <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                        <input type="password" id="password" class="form-control" name= "password" required/>
-                        <label class="form-label" for="form3Example4c">Contraseña</label>
-                        </div>
-                    </div>
-
-                    <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                        <button type="submit" id="registrarNuevo" class="btn btn-primary btn-lg">Registrar</button>
-                    </div>
-
-                </form>
-
-              </div>
-              <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-              <img src="public/img/Marca_recuerdame.png"
-                  class="img-fluid" alt="Sample image">
-
-              </div>
-            </div>
-          </div>
-        </div>
+      <div class="d-flex justify-content-center">
+        <button type="submit" id="registrarNuevo" class="btn btn-primary btn-sm">Registrar</button>
       </div>
     </div>
-  </div>
-</section>
+  </form>
 </body>
 <html>
